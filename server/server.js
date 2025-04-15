bb58f60f2904dd5e6121db128c972ae77394e09d
JUDHI3134 <judhistirbehera456@gmail.com>
2025-04-15 17:12:42 +0530

    db connection and create user model

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import connectDB from "./configs/db.js"
+import "dotenv/config"
 
 const app = express()
 const port = process.env.PORT || 4000
+
+//db connection
+await connectDB()
 
 //allow multiple origins
 const allowedOrigin = ['http://localhost:5173']
