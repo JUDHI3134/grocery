458af41e1d1a593fe99665c2fbde1d1b624e2552
JUDHI3134 <judhistirbehera456@gmail.com>
2025-04-18 18:19:09 +0530

    create product list, single product and change stock controller

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -27,26 +27,34 @@ export const addProduct = async (req, res) => {
 //List product: /api/product/list
 export const productList = async (req, res) => {
     try {
-        
+        const products = await Product.find({})
+        res.json({success: true, products}) 
     } catch (error) {
-        
+        console.log(error.message)
+        res.json({success: false, message: error.message})
     }
 }
 
 //get single product: /api/product/id
 export const productById = async (req, res) => {
     try {
-        
+        const { id } = req.body
+        const product = await Product.findById(id)
+      res.json({success: true, product})  
     } catch (error) {
-        
+        console.log(error.message)
+        res.json({success: false, message: error.message}) 
     }
 }
 
 //change product inStock: /api/product/stock
 export const changeStock = async (req, res) => {
     try {
-        
+        const { id, inStock } = req.body
+        await Product.findByIdAndUpdate(id, { inStock })
+       res.json({success: true, message: "Stock updated"}) 
     } catch (error) {
-        
+        console.log(error.message)
+        res.json({success: false, message: error.message})
     }
 }
