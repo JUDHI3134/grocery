d6cc03ec1c301c5337bb57d86b5fa28540bbb333
JUDHI3134 <judhistirbehera456@gmail.com>
2025-04-20 15:55:07 +0530

    implement add address functionality

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 
 //input field component
@@ -16,12 +18,9 @@ const InputField = ({type, placeholder, name, handleChange, address}) => (
 )
 
 
-const onSubmitHandler = async (e) => {
-
-    e.preventDefault()
-}
-
 const AddAddress = () => {
+
+    const { axios, user, navigate } = useAppContext()
 
     const [address, setAddress] = useState({
         firstname: '',
@@ -44,6 +43,28 @@ const AddAddress = () => {
         }))
     }
 
+    const onSubmitHandler = async (e) => {
+        try {
+            e.preventDefault()
+            const { data } = await axios.post("/api/address/add", {address})
+            if (data.success) {
+                toast.success(data.message)
+                navigate("/cart")
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        if (!user) {
+            navigate("/cart")
+        }
+    },[])
+
   return (
     <div className='mt-16 pb-16'>
           <p className='text-2xl md:text-3xl text-gray-500'>Add Shipping <span className='font-semibold text-primary'>Address</span></p>
@@ -53,8 +74,8 @@ const AddAddress = () => {
                   <form onSubmit={onSubmitHandler} className='space-y-3 mt-6 text-sm'>
                       
                     <div className='grid grid-cols-2 gap-4'>
-                      <InputField handleChange={handleChange} name='firstname' address={address} type='text' placeholder='First name' />    
-                      <InputField handleChange={handleChange} name='lastname' address={address} type='text' placeholder='Last name' />    
+                      <InputField handleChange={handleChange} name='firstName' address={address} type='text' placeholder='First name' />    
+                      <InputField handleChange={handleChange} name='lastName' address={address} type='text' placeholder='Last name' />    
                       </div>
                       
                       <InputField handleChange={handleChange} name='email' address={address} type='email' placeholder='Email Address' />  
